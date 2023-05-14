430ca664fbc2cb6a4b30518d81ddf79fe51f2055
Harris  N <harrisnasir8765@gmail.com>
2023-05-14 16:14:51 +0500

    removed advice gen app from repo

--- Advice-Generator-App/.netlify/server/nodes/1.js
+++
@@ -1,7 +0,0 @@
-
-
-export const index = 1;
-export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.4e5a810e.js","_app/immutable/chunks/index.cf84f0aa.js","_app/immutable/chunks/singletons.fe659880.js"];
-export const stylesheets = [];
-export const fonts = [];
